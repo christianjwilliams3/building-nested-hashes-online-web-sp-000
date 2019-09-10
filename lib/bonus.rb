f8a64efdeb9a6f383df611a68a1d7cb6ec79caa2
christian williams <christian.williamsj@gmail.com>
2019-09-10 19:11:41 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,5 +24,4 @@ epic_tragedy[:capulet][:heroine][:status]="dead"
 epic_tragedy[:montague][:hero][:status]="dead"
 epic_tragedy
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy
-end
+end 
